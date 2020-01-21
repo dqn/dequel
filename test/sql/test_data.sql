c1c1c0f0d9cd76d76e73d1169640fcf9389d994b
dqn <suyarizzz@gmail.com>
2020-01-21 13:36:05 +0900

    add: test

---
+++ test/sql/test_data.sql
@@ -0,0 +1,15 @@
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+
+DROP TABLE IF EXISTS tests CASCADE;
+
+CREATE TABLE tests (
+  id BIGSERIAL NOT NULL,
+  test_text TEXT NOT NULL DEFAULT '',
+  test_bigint BIGINT NOT NULL DEFAULT 0,
+  test_boolean BOOLEAN NOT NULL DEFAULT FALSE,
+  test_jsonb JSONB NOT NULL DEFAULT '{}',
+  test_text_array TEXT[] NOT NULL DEFAULT '{}',
+  test_timestamptz TIMESTAMPTZ NOT NULL DEFAULT '-infinity',
+  PRIMARY KEY(id)
+);
