5ab466a9a8d729012ef249c7b15a7f3e46ce9cc0
dqn <suyarizzz@gmail.com>
2020-01-22 17:27:28 +0900

    update: test

--- test/dequel.test.js
+++ test/dequel.test.js
@@ -4,7 +4,18 @@ const dequel = require('..');
 
 const fs = require('fs');
 
-class Test extends dequel.Model {}
+class Test extends dequel.Model {
+  static columns = [
+    'id',
+    'test_text',
+    'test_bigint',
+    'test_boolean',
+    'test_jsonb',
+    'test_text_array',
+    'test_timestamptz',
+  ];
+  static primaryKey = 'id';
+}
 
 const pool = new dequel.Pool({
   user: 'postgres',
@@ -23,42 +34,81 @@ async function initialize() {
   Test.init(pool);
 }
 
-beforeAll(() => {
-  return initialize();
-});
-
 afterAll(() => {
   return pool.end();
 });
 
-test('take', async () => {
-  const record = await Test.take();
-
-  expect(record).toBeDefined();
+beforeEach(() => {
+  return initialize();
 });
 
-test('count', async () => {
-  const count = await Test.count();
+describe('take', () => {
+  test('take test data', async () => {
+    const record = await Test.take();
 
-  expect(count).toBe(1);
-});
-
-test('create', async () => {
-  await Test.create({
-    test_text: 'xxx',
-    test_bigint: 42,
-    test_boolean: true,
-    test_jsonb: { foo: 'bar' },
-    test_text_array: [ 'aaa', 'bbb', 'ccc' ],
-    test_timestamptz: new Date(),
+    expect(record).toEqual(new Test({
+      id: '1',
+      test_text: 'hello',
+      test_bigint: '42',
+      test_boolean: true,
+      test_jsonb: { foo: 'bar' },
+      test_text_array: [ 'Node.js', 'PostgreSQL', 'dequel' ],
+      test_timestamptz: expect.anything(),
+    }));
   });
-  const count = await Test.count();
-
-  expect(count).toBe(2);
 });
 
-test('where', async () => {
-  const rows = await Test.where('id = $1', 1);
+describe('count', () => {
+  test('count', async () => {
+    const count = await Test.count();
 
-  expect(rows.length).toBe(1);
+    expect(count).toBe(1);
+  });
+
+  test('increment count', async () => {
+    const count = await Test.count();
+    await Test.create();
+
+    expect(await Test.count()).toBe(count + 1);
+  });
+});
+
+describe('create', () => {
+  test('create', async () => {
+    const params = {
+      test_text: 'xxx',
+      test_bigint: 23,
+      test_boolean: true,
+      test_jsonb: { foo: 'bar' },
+      test_text_array: [ 'aaa', 'bbb', 'ccc' ],
+      test_timestamptz: new Date(),
+    };
+
+    const record = await Test.create(params);
+
+    expect(record).toEqual(new Test({
+      id: '2',
+      test_text: 'xxx',
+      test_bigint: '23',
+      test_boolean: true,
+      test_jsonb: { foo: 'bar' },
+      test_text_array: [ 'aaa', 'bbb', 'ccc' ],
+      test_timestamptz: expect.anything(),
+    }));
+  });
+});
+
+describe('where', () => {
+  test('where', async () => {
+    await Promise.all([
+      Test.create({ test_bigint: 1 }),
+      Test.create({ test_bigint: 23 }),
+      Test.create({ test_bigint: 23 }),
+      Test.create({ test_bigint: 42 }),
+    ]);
+
+    const rows = await Test.where('test_bigint = $1', 23);
+
+    expect(rows.length).toBe(2);
+  });
 });
