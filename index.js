094e343b82109d771b448a61ee1fbc7308bdb447
dqn <suyarizzz@gmail.com>
2020-01-22 11:21:37 +0900

    feat: take, create

--- index.js
+++ index.js
@@ -2,10 +2,36 @@
 
 const { Pool } = require('pg');
 
+function makeBindParams(begin, count) {
+  return Array(count).fill().map((_, i) => `$${begin + i}`);
+}
+
 class Model {
-  static define(pool, attributes) {
+  static define(pool, options = {}) {
     this.pool = pool;
-    this.attributes = attributes;
+    this.tableName = options.tableName || this.name.toLowerCase() + 's';
+  }
+
+  static execute(sql) {
+    console.log('debug:', sql);
+
+    return this.pool.query(sql).then((response) => response.rows);
+  }
+
+  static take() {
+    const sql = `SELECT * FROM ${this.tableName} LIMIT 1;`;
+
+    return this.execute(sql).then((rows) => rows.shift());
+  }
+
+  static create(params) {
+    const columns = Object.keys(params);
+    const values = Object.values(params);
+
+    const bindParams = makeBindParams(1, values.length).join(',');
+    const text = `INSERT INTO ${this.tableName} (${columns.join(',')}) VALUES (${bindParams});`;
+
+    return this.execute({ text, values }).then((rows) => rows.shift());
   }
 }
 
